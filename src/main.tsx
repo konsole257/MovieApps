37f749523a4a54ed1201b2f7ac8b04fdfd90c7d0
konsole257 <konsole257@gmail.com>
2025-09-07 23:15:56 +0900

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import 'normalize.css';
@@ -10,8 +11,10 @@ import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter basename="/MovieApp">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>
 )
